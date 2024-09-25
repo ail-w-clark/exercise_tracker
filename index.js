b82905a252b2a40cc46fdd6879ca61e72fdf851a
ail-w-clark <160149680+ail-w-clark@users.noreply.github.com>
2024-09-25 13:53:16 -0400

    feat: connect to mongo

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(cors())
 app.use(express.static('public'))
