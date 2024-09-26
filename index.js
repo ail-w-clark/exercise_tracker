7f0d2430c8e0c57849d23a06a29251cdd80d3aad
ail-w-clark <160149680+ail-w-clark@users.noreply.github.com>
2024-09-26 17:24:33 -0400

    feat: add post exercise

--- index.js
+++ index.js
@@ -19,17 +19,19 @@ app.get('/', (req, res) => {
 });
 
 const exerciseSchema = new mongoose.Schema({
-  description: { type: String },
-  duration: { type: Number },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
   date: { type: String, default: () => new Date().toISOString().slice(0, 10) } 
 });
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  exercises: [exerciseSchema] 
+  count: { type: Number, default: 0 },
+  log: [exerciseSchema] 
 });
 
 const User = mongoose.model('User', userSchema);
+
 
 app.post('/api/users', (req, res) => {
   const { username } = req.body;
@@ -40,10 +42,51 @@ app.post('/api/users', (req, res) => {
 
 app.get('/api/users', async (req, res) => {
   try {
-    const users = User.find({}).select('username _id');
+    const users = await User.find({}).select('username _id');
     res.json(users);
   } catch (err) {
     res.json({ error: 'Error fetching users' });
+  }
+});
+
+app.post('/api/users/:id/exercises', async (req, res) => {
+  const { description, duration, date } = req.body;
+  const userId = req.params.id;
+
+  if (!description || !duration) {
+    return res.status(400).json({ error: 'Description and duration are required' });
+  }
+
+  const durationNumber = Number(duration);
+  if (isNaN(durationNumber)) {
+    return res.status(400).json({ error: 'Duration must be a number' });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({ error: 'User not found' });
+    }
+
+    const exercise = {
+      description, 
+      duration: durationNumber,
+      date: date ? new Date(date).toISOString().slice(0, 10) : new Date().toISOString().slice(0, 10)
+    };
+
+    user.log.push(exercise);
+    user.count += 1; 
+    user.save();
+
+    res.json({
+      username: user.username,
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date,
+      _id : user.id 
+    });
+  } catch (err) {
+    res.json({ error: "Error adding exercise"})
   }
 });
 
