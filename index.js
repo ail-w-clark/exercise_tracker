1b4a397cee6821336547ce5a64113a5102880784
ail-w-clark <160149680+ail-w-clark@users.noreply.github.com>
2024-09-26 12:47:14 -0400

    feat: add post

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+const exerciseSchema = new mongoose.Schema({
+  description: { type: String },
+  duration: { type: Number },
+  date: { type: String, default: () => new Date().toISOString().slice(0, 10) } 
+});
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  exercises: [exerciseSchema] 
+});
 
+const User = mongoose.model('User', userSchema);
 
+app.post('/api/users', (req, res) => {
+  const { username } = req.body;
+  const newUser = new User({ username });
+  newUser.save();
+  res.json(newUser);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
